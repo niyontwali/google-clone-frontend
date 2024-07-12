47945644c00f4d937a4bb28cf36c08bc6a822c23
John Niyontwali <nijohn0006@gmail.com>
2024-07-12 17:27:10 +0200

    resolved error in the search params

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -22,28 +22,28 @@ const Error: React.FC<ErrorProps> = ({ errorData }) => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center p-4 bg-gray-100">
+    <div className='min-h-screen flex flex-col justify-center items-center p-4 bg-gray-100'>
       {errorData && (
-        <p className="text-red-600 text-md mb-6 max-w-5xl text-center">
-          Error: {errorData.data?.message}
+        <p className='text-red-600 text-md mb-6 max-w-5xl text-center pt-6'>
+          {errorData.data?.message}
         </p>
       )}
       {errorData?.status === 429 ? (
-        <div className="w-full max-w-4xl grid grid-cols-1 gap-4 sm:grid-cols-3">
+        <div className='w-full max-w-4xl grid grid-cols-1 gap-4 sm:grid-cols-3'>
           {uniqueItems.map((item, index) => (
             <div
               key={index}
-              className="bg-white p-4 shadow-md rounded-lg flex items-center justify-between cursor-pointer hover:shadow-lg transition-shadow"
+              className='bg-white p-4 shadow-md rounded-lg flex items-center justify-between cursor-pointer hover:shadow-lg transition-shadow'
               onClick={() => handleCardClick(item)}
             >
-              <p className="truncate flex-1 mr-2">{item}</p>
-              <Search className="text-gray-500" size={18} /> 
+              <p className='truncate flex-1 mr-2'>{item}</p>
+              <Search className='text-gray-500' size={18} />
             </div>
           ))}
         </div>
       ) : (
         <Button
-          className="bg-primary text-white mt-4"
+          className='bg-primary text-white mt-4'
           onClick={() => window.location.reload()}
         >
           Retry
